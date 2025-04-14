2d7dd20dae78495d95429a12b61c3844201025fe
Orell Bühler <orell.buehler@ateleris.ch>
2025-04-14 18:28:02 +0200

    update data protection config

--- Ateleris.NET.Shared/Extensions/IServiceCollectionExtensions.cs
+++ Ateleris.NET.Shared/Extensions/IServiceCollectionExtensions.cs
@@ -1,13 +1,19 @@
 using Ateleris.NET.Shared.Data;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ateleris.NET.Shared.Extensions;
 
 public static class IServiceCollectionExtensions
 {
-    public static IServiceCollection SetupDataProtectionWithDbPersistance(this IServiceCollection services)
+    public static IServiceCollection SetupDataProtectionWithDbPersistance(this IServiceCollection services, string connectionString)
     {
+        services.AddDbContext<DataProtectionKeyContext>(options =>
+        {
+            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
+        });
+
         services
             .AddDataProtection()
             .PersistKeysToDbContext<DataProtectionKeyContext>();
