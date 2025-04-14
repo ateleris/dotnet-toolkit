2d629505e3bcda47ba9587403827d8e7e7e05491
Orell Bühler <orell.buehler@ateleris.ch>
2025-04-14 18:50:50 +0200

    remove data protection db context config

--- Ateleris.NET.Shared/Extensions/IServiceCollectionExtensions.cs
+++ Ateleris.NET.Shared/Extensions/IServiceCollectionExtensions.cs
@@ -1,23 +1,5 @@
-using Ateleris.NET.Shared.Data;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-
 namespace Ateleris.NET.Shared.Extensions;
 
 public static class IServiceCollectionExtensions
 {
-    public static IServiceCollection SetupDataProtectionWithDbPersistance(this IServiceCollection services, string connectionString)
-    {
-        services.AddDbContext<DataProtectionKeyContext>(options =>
-        {
-            options.UseNpgsql(connectionString).UseSnakeCaseNamingConvention();
-        });
-
-        services
-            .AddDataProtection()
-            .PersistKeysToDbContext<DataProtectionKeyContext>();
-
-        return services;
-    }
 }
