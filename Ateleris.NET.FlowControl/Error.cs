d22ae8ceb11c677835e0b1b41f5785b12cabcde3
Orell Bühler <orell.buehler@ateleris.ch>
2025-04-15 16:24:45 +0200

    create composite error structure

--- Ateleris.NET.FlowControl/Error.cs
+++ Ateleris.NET.FlowControl/Error.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
 
 namespace Ateleris.NET.FlowControl;
 
@@ -21,6 +24,8 @@ public class Error
 
 public class Error<TErrorType> : Error
 {
+    public ImmutableList<Error<TErrorType>>? ChildErrors { get; init; }
+
     public static Error<TErrorType> New(string message, TErrorType? type = default) => new()
     {
         Message = message,
@@ -39,7 +44,32 @@ public class Error<TErrorType> : Error
         Type = type
     };
 
+    public static Error<TErrorType> Composite(string message, TErrorType type, params Error<TErrorType>[] errors) => new()
+    {
+        Message = message,
+        Type = type,
+        ChildErrors = [.. errors]
+    };
+
+    public static Error<TErrorType> Composite(string message, TErrorType type, IEnumerable<Error<TErrorType>> errors) => new()
+    {
+        Message = message,
+        Type = type,
+        ChildErrors = [.. errors]
+    };
+
+    public bool IsComposite => ChildErrors is not null && ChildErrors.Count > 0;
+
     public required TErrorType? Type { get; init; } = default;
 
-    public override string ToString() => $"Error[{Type?.ToString() ?? "-"}]: {Message}";
+    public override string ToString()
+    {
+        if (!IsComposite)
+        {
+            return $"Error[{Type?.ToString() ?? "-"}]: {Message}";
+        }
+
+        return $"CompositeError[{Type?.ToString() ?? "-"}]: {Message}\n" +
+               string.Join("\n", ChildErrors!.Select((e, i) => $"  {i + 1}. {e}"));
+    }
 }
