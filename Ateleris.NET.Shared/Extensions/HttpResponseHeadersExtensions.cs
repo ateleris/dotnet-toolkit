ea050119327649c7288cfc459560889b6412c211
Orell Bühler <orell.buehler@ateleris.ch>
2025-04-15 15:21:57 +0200

    create http response header extensions

---
+++ Ateleris.NET.Shared/Extensions/HttpResponseHeadersExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Net.Http.Headers;
+
+namespace Ateleris.NET.Shared.Extensions;
+
+public static class HttpResponseHeadersExtensions
+{
+    public static string? GetHeaderValueSafe(this HttpResponseHeaders headers, string headerName)
+        => headers.GetHeaderValueSafe(headerName, val => val);
+
+    public static T? GetHeaderValueSafe<T>(this HttpResponseHeaders headers, string headerName, Func<string, T?> parseFunc)
+    {
+        if (!headers.Contains(headerName)) return default;
+        var value = headers.GetValues(headerName).FirstOrDefault();
+        return value is not null ? parseFunc(value) : default;
+    }
+}
